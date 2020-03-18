97f739ee642eeeebc6c8789cd1c95924c48e1e13
Meowton <56950678+TheMeowton@users.noreply.github.com>
2020-03-18 11:02:35 +0700

    Add files via upload

---
+++ Regular_Expressions/Lesson_26/Script.js
@@ -0,0 +1,10 @@
+// Тема 26
+document.write("Тема 26<br>");
+
+// Задача 1
+document.write("Задача 1: <br>");
+let string = 'aaa [2] bbb [3] ccc [12] ddd';
+let result = string.replace(/\[(\d+)\]/g, function(match, value){
+	return "[" + (value * value) + "]";
+});
+document.write(result);
