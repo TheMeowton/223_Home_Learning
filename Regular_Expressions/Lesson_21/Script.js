91cd0aa10d5b67f60765d2a9643f3ecdd0b95905
Meowton <56950678+TheMeowton@users.noreply.github.com>
2020-03-18 15:35:55 +0700

    Update Script.js

--- Regular_Expressions/Lesson_21/Script.js
+++ Regular_Expressions/Lesson_21/Script.js
@@ -3,6 +3,11 @@ document.write("Тема 21<br>");
 
 // Задача 1
 document.write("Задача 1: <br>");
-let string = '12 34 56 78';
-let result = string.replace(/(\d)(\d)/g, "$2$1");
+let string = '12 123 34 56 78 123 123456';
+let result = string.replace(/\b(\d)(\d)\b/g, "$2$1");
 document.write(result);
+
+/* 
+Чтобы брались только двузначные числа
+используются спец. символы \b
+*/
