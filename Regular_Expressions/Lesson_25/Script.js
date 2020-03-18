7afbe597957fa30d1582b92a77ea437d96259307
Meowton <56950678+TheMeowton@users.noreply.github.com>
2020-03-18 10:05:43 +0700

    Update Script.js

--- Regular_Expressions/Lesson_25/Script.js
+++ Regular_Expressions/Lesson_25/Script.js
@@ -4,9 +4,9 @@ document.write("Тема 25<br>");
 // Задача 2
 document.write("Задача 2: <br>");
 let string = "<a href='' class='eee' id='zzz' data-test='test'>"
-let attributes = string.match(/\w+?(?==)/g); // Без указания что атрибут пользовательский (если такие есть) прим: выведет просто test
+let attributes = string.match(/\w+?(?==)/g); // Без указания что атрибут пользовательский (если такие есть) прим: добавит просто test
 document.write("Атрибуты: " + attributes);
 
-/* С указанием что атрибут пользовательский (если такие есть) прим: выведет не test, а data-test
+/* С указанием что атрибут пользовательский (если такие есть) прим: добавит не test, а data-test
 let attributes = string.match(/\w+?-?\w*?(?==)/g);
 */
